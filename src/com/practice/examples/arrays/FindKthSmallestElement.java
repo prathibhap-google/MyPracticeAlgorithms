ad54e40b4362ddeebcd85117f1a09ddf66b8695a
Prathibha Prabhakaran <@google.com>
2017-06-13 14:05:12 -0700

    Arrays

--- src/com/practice/examples/arrays/FindKthSmallestElement.java
+++ src/com/practice/examples/arrays/FindKthSmallestElement.java
@@ -3,50 +3,53 @@ package com.practice.examples.arrays;
 public class FindKthSmallestElement {
 
     public int getKthSmallestElement(int[] arr, int k) {
-        if (arr == null || arr.length ==0 || arr.length < k-1)
+        if (arr == null || arr.length == 0 || k >= arr.length)
             return -1;
-        return getSortedArray(arr, k-1, 0, arr.length-1);
+        return getSortedArray(arr, k);
     }
 
-    public int getSortedArray(int[] arr, int k, int start, int end) {
-        int index = getPivotedArray(arr, start, end);
-        if (index == k)
-            return arr[k];
-        else if(k < index)
-            return getSortedArray(arr, k, start, index);
-        else if(k > index)
-            return getSortedArray(arr, k, index+1, end);
+    public int getSortedArray(int[] arr, int k) {
 
-        return -1;
+        int start = 0, end = arr.length - 1, index = k;
+        while (start < end) {
+            int pivot = getPivotedArray(arr, start, end);
+            if (pivot < index) start = pivot + 1;
+            else if (pivot > index) end = pivot - 1;
+            else return arr[pivot];
+        }
+        return arr[start];
     }
 
     private int getPivotedArray(int[] arr, int left, int right) {
-        int pivot = (left + right) / 2;
+        int pivot = left;
+        int temp;
 
         while (left <= right) {
 
-            while(arr[left] <= arr[pivot])
+            while(left <= right && arr[left] <= arr[pivot])
                 left++;
 
-            while(arr[right] > arr[pivot])
+            while(left <= right && arr[right] > arr[pivot])
                 right--;
 
             if(left < right) {
-                int temp = arr[left];
+                temp = arr[left];
                 arr[left] = arr[right];
                 arr[right] = temp;
-                left++;
-                right--;
             }
         }
 
-        return left;
+        temp = arr[right];
+        arr[right] = arr[pivot];
+        arr[pivot] = temp;
+
+        return right;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 5, 8, 2, 6, 9, 3};
+        int[] arr = {1, 4, 6, 2, 5, 7, 3};
         FindKthSmallestElement fElement = new FindKthSmallestElement();
-        int result = fElement.getKthSmallestElement(arr, 3);
+        int result = fElement.getKthSmallestElement(arr, 6);
         System.out.println("Smallest element: "+ result);
     }
 }
